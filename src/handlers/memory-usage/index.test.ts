2e936ff821161d0f3761b416f6c7432182c26a25
asyomei <vabgalimov@vivaldi.net>
2024-06-20 03:11:51 +0500

    Refactoring

--- src/handlers/memory-usage/index.test.ts
+++ src/handlers/memory-usage/index.test.ts
@@ -1,12 +1,12 @@
 import { describe, it } from "node:test";
 import { expect } from "expect";
+import { type ApiRequest, getTestApi } from "#/utils/testing";
 import { MemoryUsageHandler } from ".";
-import { type ApiRequest, getTestApi } from "../../utils/testing";
 
 describe("MemoryUsageHandler", () => {
   const handler = new MemoryUsageHandler();
 
-  for (const megabytes of [3, 42]) {
+  for (const megabytes of [3.45, 42]) {
     it(`should send memory usage when it is ${megabytes} MB`, async () => {
       const [api, reqs] = getTestApi();
 

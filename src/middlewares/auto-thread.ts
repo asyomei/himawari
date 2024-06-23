aef9a610e2169b2bec57301b3f721a114eead38f
asyomei <vabgalimov@vivaldi.net>
2024-06-23 22:59:35 +0500

    Fix auto thread

--- src/middlewares/auto-thread.ts
+++ src/middlewares/auto-thread.ts
@@ -3,7 +3,7 @@ import type { MiddlewareFn } from "grammy";
 export const autoThread: MiddlewareFn = async (ctx, next) => {
   const threadId = ctx.msg?.message_thread_id;
 
-  if (threadId != null) {
+  if (ctx.chat?.is_forum && threadId != null) {
     ctx.api.config.use(async (prev, method, payload, signal) => {
       payload = { message_thread_id: threadId, ...payload };
       return await prev(method, payload, signal);

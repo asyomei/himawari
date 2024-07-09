f1ada005876deaf9472f2b24593285ccbdf58a04
asyomei <vabgalimov@vivaldi.net>
2024-07-10 02:45:33 +0500

    Change privacy text

--- src/handlers/privacy.ts
+++ src/handlers/privacy.ts
@@ -5,9 +5,13 @@ import { BaseHandler } from "./base";
 const PRIVACY_TEXT = dedent`
   Данный бот предоставляет свои услуги бесплатно и хранит следующие данные для работы функционала:
   - ID пользователей и чатов - для ограничения по количеству запросов в единицу времени, для работы функционала "Чат с GPT-4"
+  - Текст сообщений пользователей функционала "Чат с GPT-4" для работы данного функционала
+  Чтобы удалить данные, связанные с функционалом "Чат с GPT-4", воспользуйтесь очисткой контекста чата (/help)
   ---
   This bot provides its services for free and stores the following data for the functionality:
   - IDs of users and chats - to limit the number of requests per unit of time, for the operation of the "Chat with GPT-4" functionality
+  - Text of messages of users of the "Chat with GPT-4" functionality for operation of this functionality
+  To remove data associated with the "Chat with GPT-4" functionality, use clear chat context (/help)
 
   Исходный код / Source code: https://github.com/asyomei/himawari-bot
 `;
@@ -20,6 +24,8 @@ export class PrivacyHandler extends BaseHandler {
   }
 
   async onPrivacyCommand(ctx: Context) {
-    await ctx.reply(PRIVACY_TEXT);
+    await ctx.reply(PRIVACY_TEXT, {
+      link_preview_options: { is_disabled: true },
+    });
   }
 }

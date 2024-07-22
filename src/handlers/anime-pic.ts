205fd0ffa45ac6b3fa5f78d7a1b4ddef52379e4d
asyomei <vabgalimov@vivaldi.net>
2024-07-22 21:03:05 +0500

    Add anime pics

---
+++ src/handlers/anime-pic.ts
@@ -0,0 +1,90 @@
+import { type Context, InlineKeyboard } from "grammy";
+import { himawari } from "#/filters/himawari";
+import {
+  type AnimePicService,
+  type GifType,
+  type PicType,
+  zGifType,
+  zPicType,
+} from "#/services/anime-pic";
+import { BaseHandler } from "./base";
+
+export class AnimePicHandler extends BaseHandler {
+  constructor(private pic: AnimePicService) {
+    super();
+
+    const msg = this.comp.on("message:text");
+    const addPic = (type: PicType, ...names: string[]) =>
+      msg.filter(himawari(type, ...names), ctx => this.onPicCommand(ctx, type));
+    const addGif = (type: GifType, ...names: string[]) =>
+      msg.filter(himawari(type, ...names), ctx => this.onGifCommand(ctx, type));
+
+    addPic("kitsune", "кицунэ", "кицуне", "anime-fox", "аниме-лиса");
+    addPic("neko", "нэко", "неко", "anime-cat", "anime-meow", "аниме-кошка", "аниме-мяу");
+    addPic("waifu", "вайфу");
+    addGif("baka", "бака");
+    addGif("bite", "кусь");
+    addGif("blush", "смущение");
+    addGif("bored", "скука");
+    addGif("cry", "плак");
+    addGif("cuddle", "тереться");
+    addGif("dance", "танцы");
+    addGif("facepalm", "рукалицо");
+    addGif("feed", "кормить");
+    addGif("handhold", "держаться-за-руки");
+    addGif("handshake", "рукопожатие");
+    addGif("happy", "ureshii", "счастье", "радость");
+    addGif("highfive", "дай-пять");
+    addGif("hug", "обнимашки");
+    addGif("kick", "пинок");
+    addGif("kiss", "поцелуй");
+    addGif("laugh", "смех");
+    addGif("lurk", "затаиться");
+    addGif("nod", "кивок");
+    addGif("nom", "ом", "ном");
+    addGif("nope", "неа");
+    addGif("pat", "гладить");
+    addGif("peck", "щечка", "щёчка");
+    addGif("poke", "тык");
+    addGif("punch", "удар");
+    addGif("shoot", "стрелять");
+    addGif("shrug", "хз");
+    addGif("slap", "пощечина", "пощёчина");
+    addGif("sleep", "споки", "спать");
+    addGif("smile", "улыбка");
+    addGif("smug", "ухмылка");
+    addGif("stare", "пристально");
+    addGif("think", "думать");
+    addGif("thumbsup", "лайк");
+    addGif("tickle", "щекотать", "щекотка");
+    addGif("wave", "махать");
+    addGif("wink", "подмигивание");
+    addGif("yawn", "зевок", "зевать");
+    addGif("yeet", "бросок");
+
+    msg.filter(himawari("anime-pic"), ctx => this.onHelpCommand(ctx));
+  }
+
+  async onPicCommand(ctx: Context, type: PicType) {
+    const pic = await this.pic.pic(type);
+    await ctx.replyWithPhoto(pic.url, {
+      caption: pic.source_url,
+    });
+  }
+
+  async onGifCommand(ctx: Context, type: GifType) {
+    const gif = await this.pic.gif(type);
+    await ctx.replyWithAnimation(gif.url, {
+      reply_markup: InlineKeyboard.from([
+        [InlineKeyboard.switchInlineCurrent(gif.anime_name, `anime ${gif.anime_name}`)],
+      ]),
+    });
+  }
+
+  async onHelpCommand(ctx: Context) {
+    const pics = [...zPicType.options, ...zGifType.options];
+    await ctx.reply(`<b>Доступные картинки (химавари &lt;команда&gt;):</b>\n${pics.join(", ")}`, {
+      parse_mode: "HTML",
+    });
+  }
+}
--- src/handlers/index.ts
+++ src/handlers/index.ts
@@ -1,8 +1,11 @@
 import type { Composer } from "grammy";
 import { AnimalPicService } from "#/services/animal-pic";
+import { AnimePicService } from "#/services/anime-pic";
 import { ChatService } from "#/services/chat";
 import { ShikimoriService } from "#/services/shikimori";
+import type { MyContext } from "#/types/context";
 import { AnimalHandler } from "./animal";
+import { AnimePicHandler } from "./anime-pic";
 import { ChatHandler } from "./chat";
 import type { HandlerDeps } from "./deps";
 import { HelpHandler } from "./help";
@@ -10,12 +13,12 @@ import { MemoryUsageHandler } from "./memory-usage";
 import { PrivacyHandler } from "./privacy";
 import { ShikimoriSearchHandler } from "./shikimori-search";
 import { unhandledUpdate } from "./unhandled-update";
-import type { MyContext } from "#/types/context";
 
 export function setupHandlers(comp: Composer<MyContext>, deps: HandlerDeps) {
   const shikimori = new ShikimoriService();
   const chat = new ChatService(deps.redis);
   const animalPic = new AnimalPicService();
+  const animePic = new AnimePicService();
 
   comp.use(
     new HelpHandler(),
@@ -23,6 +26,7 @@ export function setupHandlers(comp: Composer<MyContext>, deps: HandlerDeps) {
     new MemoryUsageHandler(),
     new ShikimoriSearchHandler(shikimori),
     new ChatHandler(chat),
+    new AnimePicHandler(animePic),
     new AnimalHandler(animalPic),
   );
 

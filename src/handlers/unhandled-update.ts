48c3b4b28b9af24687d0f966822e785a85fbbbf7
asyomei <vabgalimov@vivaldi.net>
2024-07-22 18:21:59 +0500

    Don't log unused inline query

--- src/handlers/unhandled-update.ts
+++ src/handlers/unhandled-update.ts
@@ -9,11 +9,6 @@ export async function unhandledUpdate(ctx: Context) {
     }
     await ctx.answerCallbackQuery();
   } else if (ctx.inlineQuery) {
-    const query = ctx.inlineQuery.query;
-
-    if (query !== "nop") {
-      console.log(`Callback query with data "${query}" didn't handle`);
-    }
     await ctx.answerInlineQuery([], { cache_time: 0 });
   } else if (ctx.chosenInlineResult) {
     const id = ctx.chosenInlineResult.result_id;
